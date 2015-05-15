7477a59b917cc1b6042b5d769deb152e191ea714
Evan Goldschmidt <evan@teameven.com>
2015-05-14 18:16:56 -0700

    The Great Gorp Contextification of 2015.
    While we're here, bring error handling and logging up to snuff.

--- errors.go
+++ errors.go
@@ -1,26 +1,15 @@
 package gorp
 
 import (
-	"fmt"
+	"github.com/evenco/go-errors"
 )
 
-// A non-fatal error, when a select query returns columns that do not exist
-// as fields in the struct it is being mapped to
-type NoFieldInTypeError struct {
-	TypeName        string
-	MissingColNames []string
-}
-
-func (err *NoFieldInTypeError) Error() string {
-	return fmt.Sprintf("gorp: No fields %+v in type %s", err.MissingColNames, err.TypeName)
-}
+var (
+	ErrorMismatchedSchema       = errors.Define("The database returned columns that don't exist in the model type.")
+	ErrorConcurrentModification = errors.Define("The model was updated by another agent.")
+)
 
 // returns true if the error is non-fatal (ie, we shouldn't immediately return)
 func NonFatalError(err error) bool {
-	switch err.(type) {
-	case *NoFieldInTypeError:
-		return true
-	default:
-		return false
-	}
+	return errors.Is(err, ErrorMismatchedSchema)
 }
